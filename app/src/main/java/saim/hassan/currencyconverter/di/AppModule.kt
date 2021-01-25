4097601355d399a019cc2a2d27bbea3e4757c92a
SAIM <saimh355@gmail.com>
2021-01-25 14:52:09 +0500

    Currency Converter app with MVVM

--- app/src/main/java/saim/hassan/currencyconverter/di/AppModule.kt
+++ app/src/main/java/saim/hassan/currencyconverter/di/AppModule.kt
@@ -5,6 +5,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import saim.hassan.currencyconverter.CurrencyApi
@@ -32,12 +33,12 @@ object AppModule {
     @Provides
     fun provideDispatchers(): DispatcherProvider = object : DispatcherProvider{
         override val main: CoroutineDispatcher
-            get() = TODO("Not yet implemented")
+            get() = Dispatchers.Main
         override val io: CoroutineDispatcher
-            get() = TODO("Not yet implemented")
+            get() = Dispatchers.IO
         override val default: CoroutineDispatcher
-            get() = TODO("Not yet implemented")
+            get() = Dispatchers.Default
         override val unconfined: CoroutineDispatcher
-            get() = TODO("Not yet implemented")
+            get() = Dispatchers.Unconfined
     }
 }
--- app/src/main/java/saim/hassan/currencyconverter/main/MainViewModel.kt
+++ app/src/main/java/saim/hassan/currencyconverter/main/MainViewModel.kt
@@ -3,10 +3,37 @@ package saim.hassan.currencyconverter.main
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ServiceLifecycleDispatcher
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import saim.hassan.currencyconverter.util.DispatcherProvider
 
 class MainViewModel @ViewModelInject constructor(
     private val repository: MainRepository,
     private val dispatchers: DispatcherProvider
 ):ViewModel() {
+    sealed class CurrencyEvent {
+        class Success(val resultText: String): CurrencyEvent()
+        class Failure(val errorText: String): CurrencyEvent()
+        object Loading : CurrencyEvent()
+        object Empty : CurrencyEvent()
+    }
+    private val _conversion = MutableStateFlow<CurrencyEvent>(CurrencyEvent.Empty)
+    val conversion: StateFlow<CurrencyEvent> = _conversion
+    fun convert(
+        amountStr: String,
+        fromCurrency: String,
+        toCurrency: String,
+    ){
+         val fromAmount = amountStr.toFloatOrNull()
+        if (fromAmount == null){
+            _conversion.value = CurrencyEvent.Failure("Not a valid amount")
+            return
+        }
+        viewModelScope.launch {
+            
+        }
+
+    }
 }
