a0fe50646b503414b5a1c3643603f1651be1a321
SAIM <saimh355@gmail.com>
2021-01-25 18:07:08 +0500

    Currency Converter app with MVVM

--- app/src/main/java/saim/hassan/currencyconverter/main/MainViewModel.kt
+++ app/src/main/java/saim/hassan/currencyconverter/main/MainViewModel.kt
@@ -7,7 +7,10 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import saim.hassan.currencyconverter.data.models.Rates
 import saim.hassan.currencyconverter.util.DispatcherProvider
+import saim.hassan.currencyconverter.util.Resource
+import kotlin.math.round
 
 class MainViewModel @ViewModelInject constructor(
     private val repository: MainRepository,
@@ -31,9 +34,59 @@ class MainViewModel @ViewModelInject constructor(
             _conversion.value = CurrencyEvent.Failure("Not a valid amount")
             return
         }
-        viewModelScope.launch {
-            
+        viewModelScope.launch(dispatchers.io) {
+            _conversion.value = CurrencyEvent.Loading
+            when(val ratesResponse = repository.getRates(fromCurrency)){
+                is Resource.Error -> _conversion.value = CurrencyEvent.Failure(ratesResponse.message!!)
+                is Resource.Success -> {
+                    val rates = ratesResponse.data!!.rates
+                    val rate = getRateForCurrency(toCurrency,rates)
+                    if (rate == null){
+                        _conversion.value = CurrencyEvent.Failure("Unexpected error")
+                    }else{
+                        val convertedCurrency = round(fromAmount * rate * 100) /100
+                        _conversion.value = CurrencyEvent.Success("$fromAmount $fromCurrency = $convertedCurrency $toCurrency")
+                    }
+                }
+            }
         }
 
+    }
+    private fun getRateForCurrency(currency: String,rates:Rates) = when(currency){
+
+        "AUD" -> rates.AUD
+        "BGN" -> rates.BGN
+        "EUR" -> rates.EUR
+        "BRL" -> rates.BRL
+        "CAD" -> rates.CAD
+        "CHF" ->  rates.CHF
+        "CNY" -> rates.CNY
+        "CZK" ->  rates.CZK
+        "DKK" -> rates.DKK
+        "GBP" -> rates.GBP
+        "HKD" -> rates.HKD
+        "HRK" -> rates.HRK
+        "HUF" -> rates.HUF
+        "IDR" -> rates.IDR
+        "ILS" -> rates.ILS
+        "INR" -> rates.INR
+        "ISK" -> rates.ISK
+        "JPY" -> rates.JPY
+        "KRW" -> rates.KRW
+        "MXN" -> rates.MXN
+        "MYR" -> rates.MYR
+        "NOK" -> rates.NOK
+        "NZD" -> rates.NZD
+        "PHP" -> rates.PHP
+        "PLN" -> rates.PLN
+        "RON" -> rates.RON
+        "RUB" -> rates.RUB
+        "SEK" -> rates.SEK
+        "SGD" -> rates.SGD
+        "THB" -> rates.THB
+        "TRY" -> rates.TRY
+        "USD" -> rates.USD
+        "ZAR" ->  rates.ZAR
+        else -> null
     }
 }
