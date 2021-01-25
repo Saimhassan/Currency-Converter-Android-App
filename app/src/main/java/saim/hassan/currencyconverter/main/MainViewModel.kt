46e8cf8a2ac08876e9797fe8ce4d6329ab4d379e
SAIM <saimh355@gmail.com>
2021-01-25 12:55:31 +0500

    Currency Converter app with MVVM

---
+++ app/src/main/java/saim/hassan/currencyconverter/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package saim.hassan.currencyconverter.main
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+class MainViewModel @ViewModelInject constructor():ViewModel() {
+}
