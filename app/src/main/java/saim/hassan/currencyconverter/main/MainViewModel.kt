6d94fe5cb09590377adf99a5e28740790e2b8ce0
SAIM <saimh355@gmail.com>
2021-01-25 13:26:30 +0500

    Currency Converter app with MVVM

--- app/src/main/java/saim/hassan/currencyconverter/main/MainViewModel.kt
+++ app/src/main/java/saim/hassan/currencyconverter/main/MainViewModel.kt
@@ -1,7 +1,12 @@
 package saim.hassan.currencyconverter.main
 
 import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ServiceLifecycleDispatcher
 import androidx.lifecycle.ViewModel
+import saim.hassan.currencyconverter.util.DispatcherProvider
 
-class MainViewModel @ViewModelInject constructor():ViewModel() {
+class MainViewModel @ViewModelInject constructor(
+    private val repository: MainRepository,
+    private val dispatchers: DispatcherProvider
+):ViewModel() {
 }
